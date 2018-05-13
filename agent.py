62dde879f0df3a6c2f34b1570c57d5b23c2d0d32
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2018-05-13 19:02:34 +0400

    Updated AI to be an agent class for async key press and template matching game over

--- agent.py
+++ agent.py
@@ -6,57 +6,102 @@ from grabber import grab_screen
 from image_tricks import auto_canny_params
 
 
-def process_image(image, keyboard, offset=66):
-    work_image = np.array(image)
+class CVAgent(object):
+    def __init__(self):
+        self.is_key_pressed = False
+        self.register_jump = False
+        self.jump_frames = 0
+        self.keyboard = PyKeyboard()
 
-    image_gray = cv2.cvtColor(work_image, cv2.COLOR_RGB2GRAY)
-    image_gray[:, :offset] = 255
+    def long_press_space(self):
+        """ Kind-of async long-press jump function. Agent will press space for
+        a few frames before letting go, simulating a long-press.
+        """
+        self.jump_frames -= 1
+        if self.jump_frames <= 0 and self.is_key_pressed:
+            self.is_key_pressed = False
+            self.register_jump = False
+            self.keyboard.release_key(' ')
+            return
 
-    image_blur = cv2.GaussianBlur(image_gray, (7, 7), 0)
+        if not self.is_key_pressed:
+            self.is_key_pressed = True
+            self.keyboard.press_key(' ')
+            return
 
-    lower, upper = auto_canny_params(image_blur)
-    image_edge = cv2.Canny(image_blur, lower, upper, apertureSize=3)
+        return
 
-    _, contours, _ = cv2.findContours(
-        image_edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def jump(self, closest_x, closest_y, offset):
+        """ Jump function to separate image processing and agent logic """
+        if (closest_x - offset) < 72 and not self.is_key_pressed:
+            self.register_jump = True
+            self.jump_frames = 5
 
-    closest_x = 600
-    closest_y = 0
-    for contour in contours:
-        box_x, box_y, box_w, box_h = cv2.boundingRect(contour)
+        if closest_y < 92 and not self.is_key_pressed:
+            self.register_jump = False
 
-        if box_w + box_h > 10:
-            cv2.rectangle(work_image,
-                          (box_x, box_y),
-                          (box_x + box_w, box_y + box_h),
-                          (0, 128, 0), 2)
+        if self.register_jump:
+            self.long_press_space()
 
-            if box_x < closest_x:
-                closest_x = box_x
+    def process_image(self, image, offset=66):
+        """ Agent image processing routine """
+        # Working image.
+        work_image = np.array(image)
+        # Grayed-out.
+        image_gray = cv2.cvtColor(work_image, cv2.COLOR_RGB2GRAY)
 
-            if (box_y + box_h) > closest_y:
-                closest_y = box_y + box_h
+        font = cv2.FONT_HERSHEY_PLAIN
 
-    register_jump = False
+        # Game over template.
+        go_template = cv2.imread('go_template.png')
+        go_template = cv2.cvtColor(go_template, cv2.COLOR_BGR2GRAY)
 
-    if (closest_x - offset) < 70:
-        register_jump = True
+        # Template matching.
+        tm_threshold = 1500000
+        match = cv2.matchTemplate(image_gray, go_template, cv2.TM_SQDIFF)
+        score, _, _, _ = cv2.minMaxLoc(match)
 
-    if closest_y < 92:
-        register_jump = False
+        # Check if game is over before proceeding.
+        if score < tm_threshold:
+            cv2.putText(work_image, 'Game Over', (20, 20), font, 1, (0, 0, 255))
+            return work_image
 
-    if register_jump:
-        # HACK: Needs to be event-based triggering
-        keyboard.press_key(' ')
-        time.sleep(0.15)
-        keyboard.release_key(' ')
+        # Crop off the dino part of the image.
+        image_gray[:, :offset] = 255
 
-    return work_image
+        # Detect edges of objects.
+        image_blur = cv2.GaussianBlur(image_gray, (7, 7), 0)
+        lower, upper = auto_canny_params(image_blur)
+        image_edge = cv2.Canny(image_blur, lower, upper, apertureSize=3)
+
+        # Find contours.
+        _, contours, _ = cv2.findContours(
+            image_edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Determine the closest object to dino.
+        closest_x = 600
+        closest_y = 0
+        for contour in contours:
+            box_x, box_y, box_w, box_h = cv2.boundingRect(contour)
+
+            if box_w + box_h > 10:
+                cv2.rectangle(work_image,
+                            (box_x, box_y),
+                            (box_x + box_w, box_y + box_h),
+                            (0, 128, 0), 2)
+
+                if box_x < closest_x:
+                    closest_x = box_x
+
+                if (box_y + box_h) > closest_y:
+                    closest_y = box_y + box_h
+
+        self.jump(closest_x, closest_y, offset)
+
+        return work_image
 
 
 if __name__ == "__main__":
-    k = PyKeyboard()
-
     print("Starting!")
     for i in range(5)[::-1]:
         print(i+1)
@@ -65,12 +110,13 @@ if __name__ == "__main__":
     main_window_name = "Agent"
     cv2.namedWindow(main_window_name)
     cv2.moveWindow(main_window_name, 150, 400)
+    agent = CVAgent()
 
     while True:
-        x, y = 383, 173
+        x, y = 660, 173
         im = grab_screen(x, y, x + 600, y + 150)
 
-        im_processed = process_image(im, k)
+        im_processed = agent.process_image(im)
         cv2.imshow(main_window_name, im_processed)
 
         # This needs to be here! Too fast and the OS will crash.
